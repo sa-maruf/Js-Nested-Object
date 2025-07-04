02c12b067892d4a4a8f140119bb67dbca21c84ab
unknown <Sanowar-Alam-Maruf>
2025-07-04 22:58:06 +0600

    first commit

---
+++ nested-object.js
@@ -0,0 +1,16 @@
+let pot={
+    names:'Sanowar Alam Maruf',
+    age:18,
+    profession: "Student", 
+    color:['red','green','blue','orange','white'],
+    address: { 
+        street: "123 Main Street", 
+        city: "Kishoreganj", 
+        country: "Bangladesh" 
+    }
+}
+
+console.log(pot.names);
+console.log(pot.address.city);
+console.log(pot.color[2]);
+
